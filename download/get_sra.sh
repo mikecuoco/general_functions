1b05d689d7229a59f5499f9272646f800bb8577f
mikecuoco <mcuoco@logg-cluster-1.salk.edu>
2021-09-26 12:27:14 -0700

    incorporate Rohini's code

--- download/get_sra.sh
+++ download/get_sra.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
-# Download SRA files from list
-# Input = list of SRA accessions
+#
+# Authors: Rohini Gadde & Mike Cuoco
+# Usage: ./get_sra.sh $1 $2
+#   This script iterates over an accession list to download and convert all
+#   SRA files whose accession numbers are in said list
+# $1 - Accession list of SRA files
+# $2 - Number of threads to use for fasterq-dump (dflt is 6)
+# TODO: Automate script more for future use (e.g. create file structure)
+# TODO: Automatically disable sra caching
+# TODO: Check for correct version of sra-tools
 
 set -e  
 
-# enable conda
+# check arguments
+if [[ $# -ne 2 ]]; then
+  echo "Incorrect number of arguments"
+  exit 1
+fi
+
+# Set variables
+DIR="$( cd "$( dirname "$1" )" && pwd )"
+ACC_FILE=$1
+THREADS=$2
+LOG="../failed_acc.txt"
+
+# enable conda environment if needed
 env='data'
 if ! command -v 'prefetch' &>/dev/null && \
   command -v 'conda' && \
@@ -15,41 +35,40 @@ if ! command -v 'prefetch' &>/dev/null && \
     conda activate $env
 fi
 
-DIR="$( cd "$( dirname "$1" )" && pwd )"
-ACC_FILE=$1
-THREADS=$2
-
-mkdir -p raw_fastq # create dir for fastq files
+# create dir for fastq files and enter
+mkdir -p raw_fastq 
 cd raw_fastq
+
+# Clear log before each run
+if [[ -e $LOG ]]; then
+  rm $LOG
+fi
 
 # setup numeric variables
 COUNTER=0
 TOTAL=$(wc -l ../$ACC_FILE | awk '{print $1}')
 
-while read -r ACC;
-do
+while read -r ACC; do
   # increase counter
   let COUNTER=COUNTER+1 
 
   # check for existance of file
-  if ls ${ACC}*.fastq.gz 1> /dev/null 2>&1;
-  then 
+  if [[ -e ${ACC}*.fastq.gz ]]; then 
      printf "Skipping $ACC since $(ls ${ACC}*.fastq.gz) exists...\n"
   else
-          # print in purple
+    # print in purple
     printf "\n\e[0;35mDownloading and unpacking $ACC ... [$COUNTER/$TOTAL] \e[0m"
 
     # use command group to store exit code  
     { prefetch -p $ACC && vdb-validate $ACC && fasterq-dump $ACC --split-files -f -e $THREADS -p && gzip $ACC*fastq && rm -rf $ACC; }
 
-    if [ $? -eq 0 ]
-    then
+    if [ $? -eq 0 ]; then
       # print message in green
       printf "\e[0;32m[✔] $ACC downloading and unpacking succeeded [$COUNTER/$TOTAL] \e[0m\n"
     else
       # print message in red
       printf "\e[0;31m[✖] $ACC downloading and unpacking failed \e[0m\n"
-      echo "$ACC" >> ../failed_acc.txt
+      echo "$ACC" >> $LOG
     fi
   fi
 done < ../$ACC_FILE
